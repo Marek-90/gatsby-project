6aa7c080cecb7ed6fac5320444503bc3829f51f9
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-10-05 20:50:25 +0200

    correct project

---
+++ marek-project/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+// import { Link } from "gatsby"
+
+// import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <>
+    <h1>hello Marek</h1>
+  </>
+)
+
+export default IndexPage
