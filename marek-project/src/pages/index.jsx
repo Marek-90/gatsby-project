31abbf109357db2f24a5ea8295994cde3557c454
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-10-05 21:41:59 +0200

    new window

--- marek-project/src/pages/index.jsx
+++ marek-project/src/pages/index.jsx
@@ -1,11 +1,12 @@
 import React from "react"
-// import { Link } from "gatsby"
-
-// import SEO from "../components/seo"
+import { Link } from "gatsby"
 
 const IndexPage = () => (
   <>
     <h1>hello Marek</h1>
+    <Link to={"/marek"}>
+      <button>Idź do Marka</button>
+    </Link>
   </>
 )
 
--- marek-project/src/pages/marek.jsx
+++ marek-project/src/pages/marek.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const NotFoundPage = () => (
   <>
-    <h1>Nie ma takiej strony :( </h1>
+    <h1>podstrona Marek :) </h1>
   </>
 )
 
